648ffd0dd749659d34069982d0da2bb9cc4e90f2
rafafa10 <1203rafafa@gmail.com>
2020-02-17 22:09:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appsqlite/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package com.example.appsqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "DBLogin.sql", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tbUsuario(cpf text primary key, senha text, nome text, email text, rg text, telefone text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists tbUsuario");
+    }
+    //inserindo valores no banco de dados
+
+    public boolean insert(String nome, String senha, String email, String cpf, String rg, String telefone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("cpf", cpf);
+        contentValues.put("email", email);
+        contentValues.put("senha", senha);
+        contentValues.put("nome", nome);
+        contentValues.put("rg", rg);
+        contentValues.put("telefone", telefone);
+
+        long inserido = db.insert("tbUsuario", null, contentValues);
+
+        if (inserido == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean validarEmail(String email) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("select * from tbUsuario where email = ?", new String[]{email});
+        if (cursor.getCount() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    //Verificando usuário e senha
+
+    public Boolean checarEmailSenha(String email, String senha) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from tbUsuario where email = ? and senha = ?", new String[]{email, senha});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
